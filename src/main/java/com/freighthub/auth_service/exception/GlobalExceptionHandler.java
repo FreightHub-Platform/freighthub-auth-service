7eb2d80d4a40dfc2d5b61ae841750096c0e72ae9
danujaya00 <danujaya.sandeepa@gmail.com>
2024-07-20 14:44:32 +0530

    Feat(Auth): Add Exception Handling and Api Response class

--- src/main/java/com/freighthub/auth_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/freighthub/auth_service/exception/GlobalExceptionHandler.java
@@ -1,25 +1,26 @@
 package com.freighthub.auth_service.exception;
 
+import com.freighthub.auth_service.util.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
-    public String handleRuntimeException(RuntimeException e) {
-        return e.getMessage();
+    public ResponseEntity<ApiResponse<Object>> handleRuntimeException(RuntimeException e) {
+        ApiResponse<Object> response = new ApiResponse<>(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public String handleException(Exception e) {
-        return e.getMessage();
+    public ResponseEntity<ApiResponse<Object>> handleException(Exception e) {
+        ApiResponse<Object> response = new ApiResponse<>(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
